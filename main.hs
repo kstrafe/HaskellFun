2a2c1bf1029bf504b9f0ebd7de433d5ef6e75a8e
Kevin Robert Stravers <macocio@gmail.com>
2015-03-30 09:55:26 +0200

    Correctly implement splitting

--- main.hs
+++ main.hs
@@ -7,10 +7,19 @@ merge (l:lhs) (r:rhs)
 merge [] (r:rhs) = r:rhs
 merge (l:lhs) [] = l:lhs
 
+-- Split the arrays to be sorted into tiny arrays
+split :: [Int] -> [[Int]]
+split (x:xs)
+	| length xs == 0 = [[x]]
+	| otherwise = [[x, head xs]] ++ split (tail xs)
+split [] = []
+
 main = do
 	let x = [2, 9, 10, 13]
 	let y = [1, 3, 4, 5, 12]
 	print (merge x y)
+
+	print (split [3, 9, 2, 1, 5])
 
 
 
