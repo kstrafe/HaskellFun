fa231ec721a0e2f875a31d46c43784e386e7b0b7
Kevin Robert Stravers <macocio@gmail.com>
2015-03-30 09:15:40 +0200

    Implement a list

--- main.hs
+++ main.hs
@@ -1,14 +1,13 @@
-data List = List 
+data List = List
 	{
 		element :: Int
 		, list :: List
 	}
-	| End
+	| Maybe List
 
 main = do
-	let x = List 1 (List 4 (List (-3) End))
+	let x = List 1 (List 4 (List (-3) Nothing))
 	print (element (list x))
-
 
 
 
