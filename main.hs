58d8a407df6fdcd2665296b6e61029fde09e9a2b
Kevin Robert Stravers <macocio@gmail.com>
2015-03-30 12:31:36 +0200

    At least it works now

--- main.hs
+++ main.hs
@@ -1,11 +1,47 @@
 
+
+generateRandomNumber :: Int -> Int
+generateRandomNumber seed = mod (a * seed + c) m
+	where
+		a = 1103515245
+		c = 12345
+		m = 2^31
+
+checkAnswer :: Int -> Int -> IO()
+checkAnswer correct guess
+	| correct == guess = do
+		print "Dayum son, you won!"
+		try correct (-1)
+	| otherwise = do
+		print "That's incorrect, try again"
+
+try :: Int -> Int -> IO()
+try correct tries
+	| tries == -1 = do
+		print "You won!"
+	| tries == 0 = do
+		print "Game over man..."
+	| otherwise = do
+		print "Guess the number: "
+		input <- getLine
+
+		checkAnswer correct (read input)
+
+		try correct (tries - 1)
+		print ""
+
+goIterate' :: Int -> IO()
+goIterate' seed = do
+	let number = generateRandomNumber seed
+	try 4 seed
+	goIterate' number
+
+goIterate :: IO()
+goIterate = goIterate' 124988
+
 main :: IO()
 main = do
-	print "Hey world"
+	print "Hey world, today we're gonna play guess the number.\nThis basically means that you're playing binary search.\nTo spice things up: there are only 4 tries per number.\n"
+	goIterate
 	temporary <- getLine
 	print temporary
-	if temporary == "derp" then
-		print "HEEEEEEEEY AYYYYYY"
-	else
-		print "'no'"
-		
