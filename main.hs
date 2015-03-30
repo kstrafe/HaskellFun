5192d191d4ce07ec107a1ed10b755a652b363095
Kevin Robert Stravers <macocio@gmail.com>
2015-03-30 09:47:13 +0200

    get merge working!

--- main.hs
+++ main.hs
@@ -3,12 +3,13 @@
 merge :: [Int] -> [Int] -> [Int]
 merge (l:lhs) (r:rhs)
 	| l < r = [l] ++ merge lhs (r:rhs) 
-	| otherwise = [r] ++ merge (l:lhs) rhs 
-
+	| otherwise = [r] ++ merge (l:lhs) rhs
+merge [] (r:rhs) = r:rhs
+merge (l:lhs) [] = l:lhs
 
 main = do
-	let x = [2, 9]
-	let y = [1, 3]
+	let x = [2, 9, 10, 13]
+	let y = [1, 3, 4, 5, 12]
 	print (merge x y)
 
 
