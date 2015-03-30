53a93a0616eefd69b9bad56b0111e5037f4c36e6
Kevin Robert Stravers <macocio@gmail.com>
2015-03-30 08:22:57 +0200

    Implement a basic DFS on a binary tree

--- main.hs
+++ main.hs
@@ -1,77 +1,21 @@
-import Data.Char
+data BinTree = Node {
+	bintreedescription :: String
+	, left_child :: BinTree
+	, right_child :: BinTree
+	}
+	| Empty
+	deriving (Show, Eq)
 
-factorial	::	Integer -> Integer
-factorial 		x
-	| x > 0 = x * factorial (x - 1)
-	| x == 0 = 1
-
-computeHeron a b c = sqrt (s * (s - a) * (s - b) * (s - c))
-	where
-	q = a + b + c
-	s = q / 2
-
-que a = do
-	putStrLn("This is cool")
-	print a
-
-manip :: [Integer] -> Integer
-manip list
-	| length list > 0 = head list + manip (tail list)
-	| length list == 0 = 0 + 0
-
-fib :: Integer => Integer
-fib n
-	| n == 1 = 1
-	| n == 0 = 1
-	| otherwise = do 1
-
-searchBinary' :: Int -> [Int] -> Int -> Int -> Int
-searchBinary' search_for array min max
-	| max < min = -1
-	| array!!middle < search_for = searchBinary' search_for array (middle + 1) max 
-	| array!!middle > search_for = searchBinary' search_for array min (middle - 1)
-	| otherwise = middle
-	where
-	middle = div (max + min) 2
-
-searchBinary :: Int -> [Int] -> Int
-searchBinary search_for array = searchBinary' search_for array 0 ((length array) - 1)
-
-type CardHolder = String
-type CardNumber = String
-type Address = [String]
-type CustomerID = Int
-
-data BillingInfo = CreditCard CardNumber CardHolder Address
-	| CashOnDelivery
-	| Invoice CustomerID
-	deriving (Show)
-
-data CupInfo = Cup {
-	area :: Int,
-	volume :: Float,
-	description :: String
-	}	
-	| Imaginar Int Float
-	deriving (Show)
-
-third (Imaginar _ d) = d
-
-sum' :: [Int] -> Int
-sum' (x:xs) = x + sum' xs
-sum' [] = 0
+-- Attempting to use DFS to find a specific string
+find :: BinTree -> String -> Bool
+find x y
+	| x == Empty = False
+	| (bintreedescription x) == y = True
+	| otherwise = find (left_child x) y || find (right_child x) y 
 
 main = do
-	let r = [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 22]
-	print (sum' r)
-	let cup = Cup 23 54.4 "A nice cup"
-	let cup2 = Imaginar 23 55.2
-	print cup
-	print cup2
-	let c = CreditCard "3213" "4324" ["Derp"]
-	print (third cup2);
-	print (area cup)
-
+	let x = Node "Top!" (Node "left" Empty Empty) (Node "right" Empty Empty)
+	print (find x "right")
 
 
 
