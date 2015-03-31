3a60d4797aca04171b7e01a96f95a069a1da0973
Kevin Robert Stravers <macocio@gmail.com>
2015-03-31 15:17:32 +0200

    Really clean code

--- main.hs
+++ main.hs
@@ -65,8 +65,29 @@ class N a where
 instance N Node where
 	printTree node = print node 
 
+merge :: [Int] -> [Int] -> [Int]
+merge array1@(x:xs) array2@(y:ys)
+	| x < y = [x] ++ merge xs array2
+	| otherwise = [y] ++ merge array1 ys 
+merge array1 [] = array1
+merge [] array2 = array2
+
+mergeSort :: [Int] -> [Int]
+mergeSort array@(x:xs)
+	| length array > 2 = merge (mergeSort first_half) (mergeSort second_half)
+	| length array == 2 = if x < y then [x, y] else [y, x] 
+	| length array == 1 = [x]
+	where
+		y = head xs
+		half_length = div (length array) 2
+		splitted = splitAt half_length array
+		first_half = fst splitted
+		second_half = snd splitted
+mergeSort [] = []
+
 main :: IO()
 main = do
+	print (mergeSort [1,3,4,2,-1])
 	x <- getLine
 	let y = (read x)::Int
 	print (Just 5 >>= (\x -> if (x == 0) then fail "zero" else Just (x + 1)))
