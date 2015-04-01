2482a89faf508b5302036295aa3b8af72f16714a
Kevin Robert Stravers <macocio@gmail.com>
2015-04-01 05:11:30 +0200

    How to pattern match data

--- main.hs
+++ main.hs
@@ -29,8 +29,15 @@ class What a where
 instance What I where
 	conquer input = (num input) * 2
 
+data X = X Int String
+	deriving (Show, Eq)
+
+gnam :: X -> String
+gnam (X a b) = b
+
 main :: IO()
-main = do
+main = do 
+	print $ gnam (X 0 "de")
 	print (mergeSort [1,3,4,2,-1,2,-3,9,0,1,3,-2])
 	print (conquer (I 32))
 
